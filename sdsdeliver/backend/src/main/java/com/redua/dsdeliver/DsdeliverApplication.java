8a5c73a43b67b36d08c633a4edd52613ce367bc5
Joney Pereira <joney.pereira.ext@conductor.com.br>
2022-11-15 14:58:39 -0300

    Criado a classe de configuração de segurança

--- sdsdeliver/backend/src/main/java/com/redua/dsdeliver/DsdeliverApplication.java
+++ sdsdeliver/backend/src/main/java/com/redua/dsdeliver/DsdeliverApplication.java
@@ -2,8 +2,9 @@ package com.redua.dsdeliver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class DsdeliverApplication {
 
 	public static void main(String[] args) {
