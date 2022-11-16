067f83f12b439451610040aaea7e8bda5a79b9ec
Joney Pereira <joney.pereira.ext@conductor.com.br>
2022-11-16 09:07:42 -0300

    Criando o end point put em pedidos, função pedido entregue

--- sdsdeliver/backend/src/main/java/com/redua/dsdeliver/services/OrderService.java
+++ sdsdeliver/backend/src/main/java/com/redua/dsdeliver/services/OrderService.java
@@ -40,4 +40,11 @@ public class OrderService {
         order = orderRepository.save(order);
         return new OrderDTO(order);
     }
+
+    public OrderDTO setDelivered(Long id){
+        Order order = orderRepository.getReferenceById(id);
+        order.setStatus(OrderStatus.DELIVERED);
+        order = orderRepository.save(order);
+        return new OrderDTO(order);
+    }
 }
