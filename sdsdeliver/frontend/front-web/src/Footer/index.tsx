edf6f15e708758b9b2c81ef2f905a9bcac537fd3
Joney Pereira <joney.pereira.ext@conductor.com.br>
2022-11-20 11:22:08 -0300

    Criado o rodape da pagina home

---
+++ sdsdeliver/frontend/front-web/src/Footer/index.tsx
@@ -0,0 +1,24 @@
+import './styles.css';
+import {ReactComponent as Youtube} from './yotube.svg';
+import {ReactComponent as Instagram} from './instagram.svg';
+import {ReactComponent as Linkedin} from './linkedin.svg';
+
+function Footer(){
+    return (
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            <div>
+                <a href="https://www.youtube.com/c/JoneySousa/about" target="">
+                    <Youtube />
+                </a>
+                <a href="https://www.instagram.com/joneysousapereira/" target="">
+                    <Instagram />
+                </a>
+                <a href="https://www.linkedin.com/in/joney-sousa-pereira-347aaa30/" target="">
+                    <Linkedin />
+                </a>
+            </div>
+        </footer>
+    )
+}
+export default Footer;
